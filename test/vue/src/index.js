147120823aabf458d28371aed0709c0e1798da0a
amenoyoya <mtyswe3@gmail.com>
2019-08-31 12:18:57 +0900

    del: 古いvue設定

--- test/vue/src/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import App from './App'    
-
-new Vue({
-  el: '#app', // Vueでマウントする要素
-  components: { App }, // 使用するコンポーネント
-  template: '<app/>', // el（#app）の中に表示する内容
-});
